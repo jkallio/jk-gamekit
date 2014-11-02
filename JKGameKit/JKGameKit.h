e72ba597b19873e50263e537008ca4e261ff0a89
Jussi Kallio <jkallio@gmail.com>
2014-11-02 23:51:03 +0200

    Initial Commit

---
+++ JKGameKit/JKGameKit.h
@@ -0,0 +1,13 @@
+//
+//  JKGameKit.h
+//  JKGameKit
+//
+//  Created by Jussi Kallio on 2.11.2014.
+//  Copyright (c) 2014 Kallio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JKGameKit : NSObject
+
+@end
